fe1f881f1b976ee2aaafad3aa012732b818d9fb6
Jake Kerr <152350589+jake-ise@users.noreply.github.com>
2024-06-04 11:17:27 +0900

    Add minimal example for connecting to AOAI from python

---
+++ README.md
@@ -0,0 +1,19 @@
+# Python AOAI Example
+
+This is a minimal exapmle of calling an Azure Open AI endpoint from Python.
+
+# Running
+
+- Copy `.env.example` to a file named `.env` and ddd your AOAI credentials to that file.
+
+
+- Install the python requirements listed in `requirements.txt`
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+- Run the example:
+    ```
+    python minimal-aoai.py
+    ```
---
+++ minimal-aoai.py
@@ -0,0 +1,47 @@
+import os
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+
+model = "gpt-35-turbo"
+api_version = "2024-03-01-preview"
+
+client = AzureOpenAI(
+    api_key=api_key,
+    api_version=api_version,
+    azure_endpoint=endpoint
+)
+
+history = []
+
+
+def addHistory(role, content):
+    history.append({
+        "role": role,
+        "content": content
+    })
+
+
+def queryGpt(message):
+    addHistory("user", message)
+    response = client.chat.completions.create(
+        model=model,
+        messages=history
+    )
+    response_message = response.choices[0].message
+    addHistory(response_message.role, response_message.content)
+    return response_message.content
+
+
+print(f"Chat with model {model} @ {endpoint} type \"exit\" to quit.")
+while True:
+    message = input("You: ")
+    if message.strip().lower() == "exit":
+        break
+    response = queryGpt(message)
+    print("Gpt:", response)
+
+print("Goodbye!")
