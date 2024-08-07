27aaeffa37016623496a7b63240cafd542269f1f
Jake Kerr <152350589+jake-ise@users.noreply.github.com>
2024-08-07 13:46:50 +0900

    Allow optionally connecting with DefaultAzureCredential instead of API_KEY

--- minimal-aoai.py
+++ minimal-aoai.py
@@ -1,6 +1,7 @@
 import os
 from openai import AzureOpenAI
 from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 load_dotenv()
 api_key = os.getenv("AZURE_OPENAI_API_KEY")
@@ -8,10 +9,20 @@ endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 model = os.getenv("AZURE_OPENAI_MODEL")
 api_version = os.getenv("AZURE_OPENAI_API_VERSION")
 
+
+token_provider = None
+auth_mode = "APIKey"
+if not api_key:
+    auth_mode = "DefaultAzureCredential"
+    token_provider = get_bearer_token_provider(
+        DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+    )
+
 client = AzureOpenAI(
     api_key=api_key,
     api_version=api_version,
-    azure_endpoint=endpoint
+    azure_endpoint=endpoint,
+    azure_ad_token_provider=token_provider
 )
 
 history = []
@@ -35,7 +46,7 @@ def queryGpt(message):
     return response_message.content
 
 
-print(f"Chat with model {model} @ {endpoint} type \"exit\" to quit.")
+print(f"Chat with model {model} @ {endpoint} using {auth_mode}\nPlease type \"exit\" to quit.")
 while True:
     message = input("You: ")
     if message.strip().lower() == "exit":
